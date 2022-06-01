cdaf318a55c5e3e2c17f3f8311d72940c6ba75ec
saad <rs.mailpro@gmail.com>
2022-06-01 14:08:26 +0200

    Boutons de suppression + Formulaire d'ajout

--- src/Controller/EstateController.php
+++ src/Controller/EstateController.php
@@ -10,6 +10,8 @@ use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Estate;
 use App\classe\Filter;
 use App\Form\FilterType;
+use App\Form\EstateType;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class EstateController extends AbstractController
 {
@@ -21,8 +23,49 @@ class EstateController extends AbstractController
     }
 
     #[Route('/', name: 'app_estate')]
-    public function index(Request $request): Response
+    public function index(Request $request, SluggerInterface $slugger): Response
     {
+
+        $estate = new Estate();
+        $form_add = $this->createForm(EstateType::class, $estate);
+
+        $form_add->handleRequest($request);
+
+        if($form_add->isSubmitted() && $form_add->isValid()){ // Si on esaie d'ajouter un bien immo
+            
+            $estateImage = $form_add->get('image')->getData();
+
+            if($estateImage){ // Si une image a été chargée
+
+                $originalFileName = pathinfo($estateImage->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFileName = $slugger->slug($originalFileName);
+                $newFilename = $safeFileName.'-'.uniqId().'-'.$estateImage->guessExtension();
+
+                try {
+                    $estateImage->move(
+                        $this->getParameter('image_directory'),
+                        $newFilename
+                    );
+                } catch(FileException $e){
+                    dd('Erreur');
+                }
+
+                $estate->setImage($newFilename);
+
+            }else{ // Si on a pas chargé d'image
+                $estate->setImage('img-default.jpg');
+            }
+            $this->entityManager->persist($estate);
+            $this->entityManager->flush();
+
+        }
+
+        if($request->request->get('delete-estate')){
+            $id_estate = $request->request->get('delete-estate');
+            $estate = $this->entityManager->getRepository(Estate::class)->find($id_estate);
+            $this->entityManager->remove($estate);
+            $this->entityManager->flush();
+        }
 
         $filter = new Filter();
         $form_filter = $this->createForm(FilterType::class, $filter);
@@ -45,7 +88,8 @@ class EstateController extends AbstractController
 
         return $this->render('estate/index.html.twig', [
             'estates' => $estates,
-            'form_filter' => $form_filter->createView()
+            'form_filter' => $form_filter->createView(),
+            'form_add' => $form_add->createView()
         ]);
     }
 }
